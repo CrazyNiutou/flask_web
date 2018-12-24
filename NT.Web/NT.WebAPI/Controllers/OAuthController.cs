ba56a311c9ce86e5ea8462f375bba4ed63935624
CrazyNiuTou <552463805@qq.com>
2018-12-24 21:40:51 +0800

    1

--- NT.Web/NT.WebAPI/Controllers/OAuthController.cs
+++ NT.Web/NT.WebAPI/Controllers/OAuthController.cs
@@ -4,12 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace NT.WebAPI.Controllers
 {
