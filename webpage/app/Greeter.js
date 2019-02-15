3adafcf67a510dd4974d104c5e80b14141ecd3fc
CrazyNiuTou <552463805@qq.com>
2019-02-15 20:54:49 +0800

    delete

--- webpage/app/Greeter.js
+++
@@ -1,9 +0,0 @@
-var config=require('./config.json')
-module.exports = function () {
-    mode:'production';
-    var greet = document.createElement('div');
-    greet.textContent = config.greetText;//"各位好";
-    return greet;
-}
-
-
